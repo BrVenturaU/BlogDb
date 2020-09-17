038c200132cb15aaedfbedf53b1bd76af78bb510
BrVenturaU <brandonventura16@gmail.com>
2020-09-16 20:29:39 -0600

    Add user table

--- blogDb.sql
+++ blogDb.sql
@@ -1,3 +1,24 @@
+--BRANDON MANUEL VENTUTA UMAÑA - SMIS010919
+--Creando la base de datos BlogDb
 CREATE DATABASE BlogDb;
 USE BlogDb;
+GO
+
+--Configurando el formato de la fecha para la sesión
+SET DATEFORMAT dmy;
+GO
+
+--Creando tabla Usuario
+CREATE TABLE Usuario(
+	id INT IDENTITY PRIMARY KEY NOT NULL,
+	primerNombre NVARCHAR(20) NOT NULL,
+	segundoNombre NVARCHAR(20),
+	primerApellido NVARCHAR(20) NOT NULL,
+	segundoApellido NVARCHAR(20),
+	email NVARCHAR(100) NOT NULL UNIQUE,
+	codigoUsuario NVARCHAR(20) NOT NULL UNIQUE,
+	userPassword NVARCHAR(MAX) NOT NULL,
+	fechaCreacion DATETIME CHECK(fechaCreacion <= GETDATE()),
+	tipo CHAR(2) NOT NULL
+);
 GO
