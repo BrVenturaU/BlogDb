93e07bf51cf303e45e1d4c54ae959a14496e492d
BrVenturaU <brandonventura16@gmail.com>
2020-09-18 08:45:51 -0600

    Agrega llaves foraneas y registros

--- blogDb.sql
+++ blogDb.sql
@@ -102,7 +102,8 @@ ALTER TABLE ValoracionPregunta
 	ADD FOREIGN KEY (idUsuario) REFERENCES Usuario(id);
 GO
 
---Wendy Azucena Solorzano Hernandez ***SMIS010519
+--**********************************************************************************************************************
+--Wendy Azucena Solorzano Hernandez - SMIS010519
 
 --Creacion de tablas:
 
@@ -118,6 +119,31 @@ CREATE TABLE Comentario
 );
 GO
 
+--Agregando claves externas a la tabla Comentario
+ALTER TABLE Comentario 
+	ADD FOREIGN KEY (idUsuario) REFERENCES Usuario(id);
+	
+ALTER TABLE Comentario 
+	ADD FOREIGN KEY (idPublicacion) REFERENCES Publicacion(id);
+GO
+
+--Insertando datos en tabla Comentario
+INSERT INTO Comentario VALUES
+	(1, 1, 'Muy buena información', 1, '17-08-2020');
+
+INSERT INTO Comentario VALUES
+	(1, 2, 'Las caracteristicas del tema estan erroneas', 0, '15-01-2015');
+
+INSERT INTO Comentario VALUES
+	(3, 3, 'Excelente', 0, '25-02-2018');
+
+INSERT INTO Comentario VALUES
+	(4, 1, 'Donde estan sus redes sociales...', 1, '06-11-2019');
+
+INSERT INTO Comentario VALUES
+	(5, 2, 'La mejor informacion que encontre, Gracias!', 1, '07-11-1019');
+GO
+
 --Tabla - Valoracion de Comentario
 CREATE TABLE ValoracionComentario
 (
@@ -129,6 +155,33 @@ CREATE TABLE ValoracionComentario
 );
 GO
 
+--Agregando claves externas a la tabla ValoracionComentario
+ALTER TABLE ValoracionComentario 
+	ADD FOREIGN KEY (idComentario) REFERENCES Comentario(id);
+
+ALTER TABLE ValoracionComentario
+	ADD FOREIGN KEY (idUsuario) REFERENCES Usuario(id);
+GO
+
+--Insertando datos en tabla ValoracionComentario
+INSERT INTO ValoracionComentario VALUES
+	(1, 1, 1, '17-08-2020');
+
+INSERT INTO ValoracionComentario VALUES
+	(1, 2, 0, '15-01-2015');
+
+INSERT INTO ValoracionComentario VALUES
+	(5, 2, 1, '25-02-2018');
+
+INSERT INTO ValoracionComentario VALUES
+	(5, 3, 1, '06-11-2019');
+
+INSERT INTO ValoracionComentario VALUES
+	(3, 2, 1, '07-11-1019');
+GO
+
+
+--Wendy Azucena Solorzano Hernandez - SMIS010519
 
 --TABLA - Pregunta
 CREATE TABLE Pregunta
@@ -140,6 +193,27 @@ CREATE TABLE Pregunta
 	estado BIT NOT NULL,
 	etiqueta NVARCHAR(MAX)
 );
+GO
+
+--Agregando claves externas a la tabla Pregunta
+ALTER TABLE Pregunta
+	ADD FOREIGN KEY (idCategoria) REFERENCES CategoriaPregunta(id);
+
+--Insertando datos en tabla Pregunta
+INSERT INTO Pregunta VALUES
+	(1, 'Las TIC', '¿Donde puedo aprender mas de ello?', 1, 'Tecnologia ')
+
+INSERT INTO Pregunta VALUES
+	(2, 'Las TIC', '¿Quien invento el termino TIC?', 1, 'Tecnologia ')
+
+INSERT INTO Pregunta VALUES
+	(3, 'Sobre Matrices', '¿En que se usan las matrices?', 1, 'Matematica')
+
+INSERT INTO Pregunta VALUES
+	(5, 'Metodos en C#', '¿Que significa GET y SET en C#?', 1, 'Programacion')
+
+INSERT INTO Pregunta VALUES
+	(4, 'Formularios', '¿Porque se borra todo el contenido del formario?', 1, 'Programacion')
 GO
 
 --Cristian Alexander Serrano Blanco
@@ -154,6 +228,26 @@ CREATE TABLE CategoriaPublicacion(
 );
 GO
 
+INSERT INTO CategoriaPublicacion VALUES
+	('Pelicula', 'Estrenos, chismes o rememorar peliculas',1);
+GO
+
+INSERT INTO CategoriaPublicacion VALUES
+	('Comedia', 'Memes o chistes de stand up o cualquier tipo',1 );
+GO
+
+ INSERT INTO CategoriaPublicacion VALUES
+	('Noticias', 'Engloba toda clase de noticias de economia,deporte,enfermedades y acontecimientos naturales',1);
+GO
+
+INSERT INTO CategoriaPublicacion VALUES
+	('Noticias', 'Engloba toda clase de noticias de economia,deporte,enfermedades y acontecimientos naturales',1 );
+GO
+
+INSERT INTO CategoriaPublicacion VALUES
+	('Videojuegos', 'Actualizaciones y DLC de nuevos o viejos videojuegos',1 );
+GO
+
 --Creacion de la tabla CategoriaPregunta
 CREATE TABLE CategoriaPregunta(
 	id TINYINT IDENTITY PRIMARY KEY NOT NULL,
@@ -163,12 +257,32 @@ CREATE TABLE CategoriaPregunta(
 );
 GO
 
+INSERT INTO CategoriaPregunta VALUES
+	('¿Como Iniciar Sesion?', 'Obtén información sobre cómo iniciar sesión en tu cuenta',1);
+GO
+
+INSERT INTO CategoriaPregunta VALUES
+	('¿como registrarse?', 'Obtén información sobre cómo registrar tu cuenta',1);
+GO
+
+INSERT INTO CategoriaPregunta VALUES
+	('¿como puedo comentar en un blog?', 'Obtén información sobre cómo comentar un blog',1);
+GO
+
+INSERT INTO CategoriaPregunta VALUES
+	('¿Como cerrar mi cuenta definitivamente?', 'Obtén información sobre cómo comentar un blog',1);
+GO
+
+INSERT INTO CategoriaPregunta VALUES
+	('¿Como valorar un comentario?', 'Obtén información sobre cómo comentar un blog',1);
+GO
+
 
 --Creacion de la tabla Publicacion
 
 CREATE TABLE Publicacion(
 	id INT IDENTITY PRIMARY KEY NOT NULL,
-	idCategoria INT NOT NULL,
+	idCategoria SMALLINT NOT NULL,
 	idUsuario INT NOT NULL,
 	titulo NVARCHAR(150) NOT NULL,
 	cabecera NVARCHAR(MAX) NOT NULL,
@@ -178,4 +292,34 @@ CREATE TABLE Publicacion(
 	estado BIT NOT NULL,
 	imagen NVARCHAR(MAX),
 );
+GO
+
+--Cristian Alexander Serrano Blanco
+--Agregando llaves fóraneas a Publicacion
+
+ALTER TABLE Publicacion
+	ADD FOREIGN KEY (idUsuario) REFERENCES Usuario(id);
+GO
+
+ALTER TABLE Publicacion
+	ADD FOREIGN KEY (idCategoria) REFERENCES CategoriaPublicacion(id);
+GO
+
+
+--Insertar datos en la tabla Publicacion
+INSERT INTO Publicacion VALUES
+	(1,1,'VALORANT EL NUEVO JUEGO COMPETITIVO', 'Encuentra todo lo que debes saber de la nueva sensación
+	en los eSport','Aqui tuviera que ir toda la publicidad y informacion de el juego pero como
+	no quise buscarlo en la pagina mejor preferi escribir lo primero que se viniera a la cabeza gracias por 
+	aver leido hasta aquí, fue una bonita forma de perder tu tiempo. :) ', '16-09-2020', '16-09-2020',1);
+GO
+
+INSERT INTO Publicacion VALUES
+	(1,1,'SE DESCUBRE FORMA MILAGROSA DE NO CONTRAR EL COVID DESCUBRELO AQUI...', 'Investigadores de Donald Trump aseguran la constante injesta de...','
+	Este espacio es solo para decirte que es totalmente para que te distraigas y que leas esto aunque no quieras', '16-09-2020', '16-09-2020',1);
+GO
+
+INSERT INTO Publicacion VALUES
+	(1,1,'ESTRENO, ESTRENO LA NUEVA PELICULA QUE REVOLUCIONARÁ EL 2020', 'la pelicula tiene por titulo the sad little bee','
+	Tu tandras el deber de descubrir porque esta triste solo en cines el 24 de septiembre', '16-09-2020', '16-09-2020',1);
 GO
